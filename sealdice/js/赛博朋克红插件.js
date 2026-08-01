2d1a1b3098c8998ce4ca450f975e91feb9a50c76
Fehling0v0 <2480400632@qq.com>
2026-08-01 20:16:17 +0800

    Update 赛博朋克红插件.js

--- sealdice/js/赛博朋克红插件.js
+++ sealdice/js/赛博朋克红插件.js
@@ -6,8 +6,8 @@
 // @timestamp    
 // 2026/8/1
 // @license      Apache-2
-// @homepageURL  
-// @updateUrl   
+// @homepageURL  https://raw.githubusercontent.com/Fehling0v0/TRPG/refs/heads/main/sealdice/js/%E8%B5%9B%E5%8D%9A%E6%9C%8B%E5%85%8B%E7%BA%A2%E6%8F%92%E4%BB%B6.js
+// @updateUrl    https://github.com/Fehling0v0/TRPG/blob/main/sealdice/js/%E8%B5%9B%E5%8D%9A%E6%9C%8B%E5%85%8B%E7%BA%A2%E6%8F%92%E4%BB%B6.js
 // ==/UserScript==
 
 
